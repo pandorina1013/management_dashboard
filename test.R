81d73a154fe3399650972fc2ff291b7ccdbb8365
Kenta Yamada <pandorina463@gmail.com>
2017-12-25 12:56:33 +0900

    今日までグラフを引っ張る

--- test.R
+++ test.R
@@ -5,16 +5,30 @@ library(formattable)
 library(dplyr)
 library(plotly)
 
-output.dt <- fread("data.csv", encoding = "UTF-8")
-outcome.dt <- output.dt %>% filter(amount < 0) %>% select(ds,amount)
-outcome.dt <- outcome.dt[as.character(substr(Sys.Date(),1,4)) == as.character(substr(outcome.dt$ds,1,4)),]
-outcome.dt$ds <- substr(outcome.dt$ds,6,7)
-outcome.dt <- outcome.dt %>% group_by(ds) %>% summarise(total = sum(amount))
+asset.dt <- output.dt %>% 
+  select(ds, amount, details) %>% 
+  data.table(. , tmp = paste0(.$ds ,.$details)) %>% 
+  group_by(tmp) %>% 
+  summarise(total = sum(amount))
 
-income.dt <- output.dt %>% filter(amount >= 0) %>% select(ds,amount)
-income.dt <- income.dt[as.character(substr(Sys.Date(),1,4)) == as.character(substr(income.dt$ds,1,4)),]
-income.dt$ds <- substr(income.dt$ds,6,7)
-income.dt <- income.dt %>% group_by(ds) %>% summarise(total = sum(amount))
+asset.dt <-  asset.dt %>% 
+  data.table(ds = as.Date(substr(.$tmp, 1, 10)), amount = .$total, details = substr(.$tmp, 11, 20)) %>% 
+  dcast(ds ~ details, value.var = "amount") %>% 
+  data.frame()
+asset.dt[is.na(asset.dt)] <- 0
 
-i_o.dt <- data.table(income = income.dt, outcome = -outcome.dt$total, diff = income.dt$total+outcome.dt$total)
-colnames(i_o.dt) <- c("ds","income","outcome","diff")
+
+for(i in 1:nrow(asset.dt)){
+  for(j in 2:ncol(asset.dt))
+    if(i != 1){
+      asset.dt[i,j] <- asset.dt[i-1,j] + asset.dt[i,j]
+    }
+}
+for(i in 1:nrow(asset.dt)){
+  for(j in 3:ncol(asset.dt)){
+    asset.dt[i,j] <- asset.dt[i,j-1] + asset.dt[i,j]
+  }
+}
+
+asset.dt <- asset.dt %>% rbind(., .[nrow(.),])
+asset.dt$ds[nrow(asset.dt)] <- Sys.Date() %>% as.Date()
